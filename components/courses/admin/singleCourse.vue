553c808f243e8539dfd1e4dfdb1dec169221ad33
Tomasz_Wadrzyk <tomulus91@gmail.com>
2018-07-04 23:56:14 +0200

    single course

--- components/courses/admin/singleCourse.vue
+++ components/courses/admin/singleCourse.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        {{this.params}}
+        <h1>{{ this.courseData.name }}</h1>
     </div>
 </template>
 
@@ -9,6 +9,44 @@ export default {
   name: 'singleCourse',
   props: [
     'params'
-  ]
+  ],
+  data () {
+    return {
+      courseData: {}
+    }
+  },
+  mounted () {
+    this.setCourseData()
+  },
+  methods: {
+    setCourseData () {
+      this.params.saveCourses.forEach((saveCourse) => {
+        if (saveCourse._id === this.params.idCourse) {
+          this.courseData.id = saveCourse._id
+          this.params.users.forEach((user) => {
+            if (user.token === saveCourse.user) {
+              this.courseData.nameUser = user.name
+              this.courseData.tokenUser = user.token
+              return false
+            }
+          })
+          this.params.courses.forEach((course) => {
+            if (course.token === saveCourse.course) {
+              this.courseData.address = course.address
+              this.courseData.duration = course.duration
+              this.courseData.begin = course.begin
+              this.courseData.price = course.price
+              this.courseData.link = course.link
+              this.courseData.name = course.name
+              this.courseData.information = course.information
+              return false
+            }
+          })
+          return false
+        }
+      })
+      console.log(this.courseData)
+    }
+  }
 }
 </script>

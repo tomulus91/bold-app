34a05e2554ad13db3f61482eaf1b0c6cbdb29641
Tomasz_Wadrzyk <tomulus91@gmail.com>
2018-07-09 14:08:41 +0200

    ustawienia

--- components/settingsApplication/index.vue
+++ components/settingsApplication/index.vue
@@ -1,23 +1,34 @@
 <template>
     <div>
-        <h1>Ustawienia aplikacji 2</h1>
-        <div>
-            Administratorzy systemu
+        <h1>{{ this.titleSetting }}</h1>
+        <div content="all-setting" v-if="!this.showSingleSetting">
+            <div @click="setSettingToView('keyAdmin')">
+                Administratorzy systemu
+            </div>
+            <div @click="setSettingToView('courseSetting')">
+                Ustawienia szkoleń
+            </div>
         </div>
-        <div>
-            Ustawienia szkoleń
-        </div>
+        <div v-if="this.showSingleSetting" @click="setView">Wyjdź</div>
+        <single-view :dataSetting="dataForSingleView" v-if="this.showSingleSetting" />
     </div>
 </template>
 
 <script>
 import settingsApplicationService from '@/assets/service/settingsApplication'
+import singleView from '@/components/settingsApplication/singleSetting.vue'
 
 export default {
   name: 'settingApplication',
+  components: {
+    singleView
+  },
   data () {
     return {
-      settingsData: {}
+      settingsData: {},
+      showSingleSetting: false,
+      dataForSingleView: {},
+      titleSetting: 'Ustawienia aplikacji'
     }
   },
   beforeMount () {
@@ -28,10 +39,31 @@ export default {
       const settingsPromise = settingsApplicationService.fetchSettings()
       settingsPromise.then((result) => {
         if (result.data) {
-          console.log(result.data)
           this.settingsData = result.data
         }
       })
+    },
+    setView () {
+      this.showSingleSetting = !this.showSingleSetting
+      if (!this.showSingleSetting) {
+        this.titleSetting = 'Ustawienia aplikacji'
+      }
+    },
+    setSettingToView (searchKey) {
+      switch (searchKey) {
+        case 'keyAdmin':
+          this.titleSetting = 'Administratorzy systemu'
+          break
+        case 'courseSetting':
+          this.titleSetting = 'Szkolenia - ustawienia'
+          break
+        default:
+          this.titleSetting = 'Ustawienia aplikacji'
+      }
+      this.dataForSingleView = this.settingsData.filter((index) => {
+        return index.keyOption === searchKey
+      })
+      this.setView()
     }
   }
 }
---
+++ components/settingsApplication/singleSetting.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-if="dataSetting.length > 0" class="table-wrap">
+            <table>
+                <tr>
+                    <td>Nazwa</td>
+                    <td>Opcje</td>
+                </tr>
+                <tr v-for="setting in dataSetting">
+                    <td>{{ setting.valueOption }}</td>
+                    <td align="center">
+                        <a href="#" @click.prevent="editUser(user.token)">Edit</a> |
+                        <a href="#" @click.prevent="deleteUser(user.token)">Delete</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'singleSetting',
+  props: [
+    'dataSetting'
+  ]
+}
+</script>
